d41063444982e7739bf2328f8ec5a5859de40d3b
JagathVM <17jagathvm@gmail.com>
2024-12-14 19:39:44 +0530

    Feat: Add admin-user-profile.js to handle user block/unblock functionality on admin side

---
+++ public/admin/js/utils/admin-user-profile.js
@@ -0,0 +1,24 @@
+import HttpRequest from "../../../helpers/http-request.js";
+import { showToast, errorMessage } from "../../../helpers/toast.js";
+import { handleReload } from "../../../helpers/handleUrl.js";
+
+const blockUnblockForm = document.getElementById("blockUnblockForm");
+
+blockUnblockForm.addEventListener("submit", async (e) => {
+  e.preventDefault();
+  const username = blockUnblockForm.getAttribute("data-username");
+  const formData = new FormData(blockUnblockForm);
+  const data = Object.fromEntries(formData);
+
+  try {
+    const apiClient = new HttpRequest("/admin/user-profile");
+    const { success, message } = await apiClient.patch(`/${username}`, data);
+
+    if (!success) return showToast(message || errorMessage, false);
+    showToast(message, true, "center", "top");
+    handleReload();
+  } catch (error) {
+    console.error(error);
+    showToast(errorMessage, false);
+  }
+});
