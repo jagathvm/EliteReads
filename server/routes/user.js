eb385fb32a65941e3359a8a07e264f03397bcf91
JagathVM <17jagathvm@gmail.com>
2025-03-27 16:19:14 +0530

    Feat: Add user address and cart management routes

--- server/routes/user.js
+++ server/routes/user.js
@@ -4,6 +4,11 @@ import {
   checkUserLoggedIn,
 } from "../middlewares/authenticate.js";
 import * as userController from "../controllers/userController.js";
+import validateData from "../middlewares/validateData.js";
+import {
+  postAddressValidationSchema,
+  editAddressValidationSchema,
+} from "../validators/userSchema.js";
 
 const router = Router();
 
@@ -19,8 +24,28 @@ router.get("/purchase-guide", userController.getUserPurchaseGuide);
 
 router.use(authenticateToken);
 router.get("/profile", userController.getUserProfile);
+router.get("/profile/address", userController.getUserAddress);
+router.get("/profile/orders", userController.getUserOrders);
+
+router.post(
+  "/profile/address",
+  validateData(postAddressValidationSchema),
+  userController.postUserAddress
+);
+router.patch(
+  "/profile/address/:addressId",
+  validateData(editAddressValidationSchema),
+  userController.editUserAddress
+);
+router.delete("/profile/address/:addressId", userController.deleteUserAddress);
+
 router.get("/readlist", userController.getUserReadlist);
 router.post("/readlist", userController.postUserReadlist);
 router.get("/cart", userController.getUserCart);
+router.post("/cart", userController.postUserCart);
+router.put("/cart/quantity-update", userController.putUserCartQuantity);
+router.put("/cart/remove", userController.removeBookFromCart);
+router.get("/cart/checkout", userController.getUserCheckout);
+router.post("/cart/place-order", userController.postUserPlaceOrder);
 
 export default router;
