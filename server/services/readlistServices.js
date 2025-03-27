9866b9968ec57fdaf0b3a63bb97b37561e90811b
JagathVM <17jagathvm@gmail.com>
2025-03-27 16:29:18 +0530

    Added readlist aggregation functions with book details lookup and optimized data retrieval.

--- server/services/readlistServices.js
+++ server/services/readlistServices.js
@@ -44,3 +44,87 @@ export const updateReadlistData = async (query, operation) => {
     console.error(`Error updating readlist`, error);
   }
 };
+
+// Data aggregation and fetching
+export const getAggregatedReadlist = async (pipeline) => {
+  try {
+    const readlist = await getAggregatedDocuments(
+      pipeline,
+      getReadlistCollection,
+      "Error retrieving readlist."
+    );
+
+    return readlist;
+  } catch (error) {
+    console.error(`Error retrieving readlist: ${error}`);
+    throw error;
+  }
+};
+
+export const fetchAggregatedReadlistData = async (userId) => {
+  const readlistData = await getReadlistByUserId(userId);
+
+  if (!readlistData) return {};
+  if (readlistData?.books?.length === 0) return readlistData;
+
+  const pipeline = [
+    {
+      $match: { userId },
+    },
+    {
+      $unwind: "$books",
+    },
+    {
+      $lookup: {
+        from: "books",
+        localField: "books.bookId",
+        foreignField: "_id",
+        as: "bookDetails",
+      },
+    },
+    {
+      $unwind: "$bookDetails",
+    },
+    {
+      $addFields: {
+        "books.bookDetails": "$bookDetails",
+      },
+    },
+    {
+      $project: {
+        _id: 1,
+        userId: 1,
+        books: {
+          bookId: "$books.bookId",
+          title: "$bookDetails.title",
+          author: "$bookDetails.author",
+          coverImageUrls: {
+            $arrayElemAt: ["$bookDetails.coverImageUrls", 0],
+          },
+          price: "$bookDetails.price",
+          bookQuantity: "$bookDetails.quantity",
+        },
+      },
+    },
+    {
+      $group: {
+        _id: "$_id",
+        userId: {
+          $first: "$userId",
+        },
+        books: {
+          $push: "$books",
+        },
+      },
+    },
+  ];
+
+  try {
+    const { value } = await getAggregatedCart(pipeline);
+    const cart = value?.[0] || {};
+    return cart;
+  } catch (error) {
+    console.error("Error fetching cart data", error);
+    throw error;
+  }
+};
