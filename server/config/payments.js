76b1359f295e7ab92a4d5fdc8cad8ee01c4a9d14
JagathVM <17jagathvm@gmail.com>
2025-04-29 19:49:03 +0530

    Add payments.js to configure Razorpay and PayPal clients

---
+++ server/config/payments.js
@@ -0,0 +1,23 @@
+import Razorpay from "razorpay";
+import { Client, Environment, LogLevel } from "@paypal/paypal-server-sdk";
+
+const client = new Client({
+  clientCredentialsAuthCredentials: {
+    oAuthClientId: process.env.PAYPAL_CLIENT_ID,
+    oAuthClientSecret: process.env.PAYPAL_CLIENT_SECRET,
+  },
+  timeout: 0,
+  environment: Environment.Sandbox,
+  logging: {
+    logLevel: LogLevel.Info,
+    logRequest: { logBody: true },
+    logResponse: { logHeaders: true },
+  },
+});
+
+export const razorpay = new Razorpay({
+  key_id: process.env.RAZORPAY_KEY_ID,
+  key_secret: process.env.RAZORPAY_KEY_SECRET,
+});
+
+export const paypalClient = () => client;
