a96d95b70c2e9154dd6403d417b204283ae24d19
JagathVM <17jagathvm@gmail.com>
2025-04-29 17:35:08 +0530

    Add handleInstantRedirect to handleUrl.js for immediate navigation

--- public/helpers/handleUrl.js
+++ public/helpers/handleUrl.js
@@ -3,5 +3,7 @@ export const handleRedirect = (url) =>
     window.location.href = url;
   }, 2000);
 
+export const handleInstantRedirect = (url) => (window.location.href = url);
+
 export const handleReload = () =>
   setTimeout(() => window.location.reload(), 2000);
