d47455fd0f85ecfe79f41ea5720f651fc082f0cf
JagathVM <17jagathvm@gmail.com>
2025-04-29 19:50:13 +0530

    Add paymentHelper.js with Razorpay signature verification and receipt generation

---
+++ server/helpers/paymentHelper.js
@@ -0,0 +1,16 @@
+import crypto, { randomUUID } from "crypto";
+
+export const verifyRazorpaySignature = ({
+  razorpay_payment_id,
+  razorpay_order_id,
+  razorpay_signature,
+}) => {
+  const generatedSignature = crypto
+    .createHmac("sha256", process.env.RAZORPAY_KEY_SECRET)
+    .update(`${razorpay_order_id}|${razorpay_payment_id}`)
+    .digest("hex");
+
+  return generatedSignature === razorpay_signature;
+};
+
+export const generateReceipt = () => randomUUID();
